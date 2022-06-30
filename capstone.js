8aac36a5f109126a3f31909b3e3a9332cc7fb1e1
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-06-30 08:43:43 -0300

    js cards

--- capstone.js
+++ capstone.js
@@ -1,4 +1,3 @@
-
 const hamburguer = document.querySelector('.hamburguer');
 const menu = document.querySelector('#nav-menu');
 const body = document.querySelector('body');
@@ -12,239 +11,107 @@ hamburguer.addEventListener('click', () => {
   body.classList.toggle('stopscroll');
 });
 
-const speaker1 = [
-    {
+//----Speaker cards------
+const speakers = document.querySelector(".speakers-cards");
+const speakerCardsArray = 
+    [{
       heading: 'Young Mog',
       image: 'assets/YoungMog.jpg',
-      badge1: 'Sungkyunkwan University School of Medicine',
+      badge1: 'Sungkyunkwan University of Medicine',
       badge2: 'South Korea',
-      image: 'assets/dottedSquare.png',
+      image2: 'assets/dottedSquare.png',
   },
-];
-const speaker2 = [
+
   {
     heading: 'Sergey Suchkov',
     image: 'assets/SergeySuchkov.jpg',
     badge1: 'Sechenov University',
     badge2: 'Russia',
-    image: 'assets/dottedSquare.png',
+    image2: 'assets/dottedSquare.png',
 },
-];
-const speaker3 = [
-  {
+
+ {
     heading: 'Donato Bonifazi',
     image: 'assets/DonatoBonifazi.jpg',
-    badge1: 'Consortium for Biological and Pharmacological Evaluations',
+    badge1: 'Biological and Pharmacological Evaluations',
     badge2: 'Italy',
-    image: 'assets/dottedSquare.png',
+    image2: 'assets/dottedSquare.png',
 },
-];
-const speaker4 = [
+
   {
     heading: 'Antonino Araco',
-    image: 'assets/DonatoBonifazi.jpg',
+    image: 'assets/AntoninoAraco.jpg',
     badge1: 'University of Camerino and Torino',
     badge2: 'Italy',
-    image: 'assets/dottedSquare.png',
+    image2: 'assets/dottedSquare.png',
 },
-];
-const speaker5 = [
-  {
+
+{
     heading: 'Federico Benetti',
     image: 'assets/FedericoBenetti.jpg',
     badge1: 'Benetty Foundation',
     badge2: 'Argentina',
-    image: 'assets/dottedSquare.png',
+    image2: 'assets/dottedSquare.png',
 },
 ];
 
-const main = document.getElementById('main')
-const container = document.createElement('div');
-container.classList = 'featured-speakers';
-main.appendChild(container);
+function speaker() {
+  let speakerCard = `<ul class="cards">`;
+  for (let speakerCards = 0;
+    speakerCards < speakerCardsArray.length;
+        speakerCards += 1
+    ){
+    const card =
+          `<li class="speaker">
+            <div class "sp">
+            <div class="square">
+              <img class="dot" src="${speakerCardsArray[speakerCards].image2}">
+              <img class="speakerimg" src="${speakerCardsArray[speakerCards].image}" alt='speaker image'>
+              </div>
+          <div class="speak">
+            <h5 class="speaker-title">${speakerCardsArray[speakerCards].heading}</h5>
+            <p class="speaker-subt">${speakerCardsArray[speakerCards].badge1}</p>
+            <p class="speaker-descript">${speakerCardsArray[speakerCards].badge2}</p>
+          </div>
+          </div>
+          </li>`
+      speakerCard += card;
+  }
+  speakerCard += `</ul>`;
+  speakers.innerHTML = speakerCard;
+}
+speaker();
+  
+// -----------FEATURED SPEAKERS----------//
+{/* const speakers=document.querySelector(".speakers")
 
-const speakersCards = document.createElement('div');
-speakersCards.classList = 'speakers-cards';
-container.appendChild(speakersCards)
+const speakerDetails = [
+  {
+    speakerbg: "../assets/speakers/chess.png",
+    speakerimage: "../assets/speakers/oprah.jpg",
+    speakername: "Oprah Gail Winfrey",
+    firstintro:
+      "Oprah Winfrey an American talk  , actress, author, and philanthropist.",
+    secondintro:
+      "Dubbed the Queen of All Media, was once the world's only black billionaire.",
+  },
 
-const cards = document.createElement('ul');
-cards.classList = 'cards';
-speakersCards.appendChild(cards);
+  {
+    speakerbg: "../assets/speakers/chess.png",
+    speakerimage: "../assets/speakers/Dangote.jpg",
+    speakername: "Aliko Dangote",
+    firstintro:
+      "He is a billionaire business magnate.CEO of the Dangote Group.",
+    secondintro:
+      "Dangote became Nigeria's first billionaire in 2007.",
+  },
 
-speaker1.classList = 'speaker 1';
-cards.appendChild(speaker1);
+  {
+    speakerbg: "../assets/speakers/chess.png",
+    speakerimage: "../assets/speakers/gates.jpg",
+    speakername:"Bill Gates",
+    firstintro:"He is business magnate philanthropist and co-founder of Microsoft",
+    secondintro: "Bill Gates has he
 
-const square = document.createElement('div')
-square.classList = 'square';
-cards.appendChild(square);
-
-const img = document.createElement('img')
-img.classList = 'dot';
-square.src = (speaker1[4].image);
-square.appendChild(img);
-
-const imgSP1 = document.createElement('img');
-imgSP1.classList = 'speakerimg';
-imgSP1.src = (speaker1[1].image);
-cards.appendChild(imgSP1);
-
-const speak = document.createElement('div');
-speak.classList = 'speak';
-speaker1.appendChild(speak);
-
-const speakTitle = document.createElement('h5');
-speakTitle.classList = 'speaker-title';
-speakTitle.innerText = speaker1[0].heading
-speak.appendChild(speakTitle);
-
-const speakSub = document.createElement('p');
-speakSub.classList = 'speaker-subt';
-speakSub.innerHTML = speaker1[2].badge1;
-speak.appendChild(speakSub);
-
-const speakerDescr = document.createElement('p');
-speakerDescr.classList = 'speaker-descript';
-speakerDescr.innerHTML = speaker1[3].badge2;
-speak.appendChild(speakerDescr);
-
-speaker2.classList = 'speaker 2';
-cards.appendChild(speaker2);
-
-square.classList = 'square';
-cards.appendChild(square);
-
-const img = document.createElement('img')
-img.classList = 'dot';
-square.src = (speaker2[4].image);
-square.appendChild(img);
-
-const imgSP2 = document.createElement('img');
-imgSP2.classList = 'speakerimg';
-imgSP2.src = (speaker2[1].image);
-cards.appendChild(imgSP2);
-
-const speak = document.createElement('div');
-speak.classList = 'speak';
-speaker1.appendChild(speak);
-
-const speakTitle = document.createElement('h5');
-speakTitle.classList = 'speaker-title';
-speakTitle.innerText = speaker2[0].heading
-speak.appendChild(speakTitle);
-
-const speakSub = document.createElement('p');
-speakSub.classList = 'speaker-subt';
-speakSub.innerHTML = speaker2[2].badge1;
-speak.appendChild(speakSub);
-
-const speakerDescr = document.createElement('p');
-speakerDescr.classList = 'speaker-descript';
-speakerDescr.innerHTML = speaker2[3].badge2;
-speak.appendChild(speakerDescr);
-
-speaker3.classList = 'speaker 3';
-cards.appendChild(speaker3);
-
-square.classList = 'square';
-cards.appendChild(square);
-
-const img = document.createElement('img')
-img.classList = 'dot';
-square.src = (speaker3[4].image);
-square.appendChild(img);
-
-const imgSP3 = document.createElement('img');
-imgSP3.classList = 'speakerimg';
-imgSP3.src = (speaker3[1].image);
-cards.appendChild(imgSP3);
-
-const speak = document.createElement('div');
-speak.classList = 'speak';
-speaker1.appendChild(speak);
-
-const speakTitle = document.createElement('h5');
-speakTitle.classList = 'speaker-title';
-speakTitle.innerText = speaker3[0].heading
-speak.appendChild(speakTitle);
-
-const speakSub = document.createElement('p');
-speakSub.classList = 'speaker-subt';
-speakSub.innerHTML = speaker3[2].badge1;
-speak.appendChild(speakSub);
-
-const speakerDescr = document.createElement('p');
-speakerDescr.classList = 'speaker-descript';
-speakerDescr.innerHTML = speaker3[3].badge2;
-speak.appendChild(speakerDescr);
-
-speaker4.classList = 'speaker 4';
-cards.appendChild(speaker4);
-
-square.classList = 'square';
-cards.appendChild(square);
-
-const img = document.createElement('img')
-img.classList = 'dot';
-square.src = (speaker1[4].image);
-square.appendChild(img);
-
-const imgSP4 = document.createElement('img');
-imgSP4.classList = 'speakerimg';
-imgSP4.src = (speaker4[1].image);
-cards.appendChild(imgSP4);
-
-const speak = document.createElement('div');
-speak.classList = 'speak';
-speaker1.appendChild(speak);
-
-const speakTitle = document.createElement('h5');
-speakTitle.classList = 'speaker-title';
-speakTitle.innerText = speaker4[0].heading
-speak.appendChild(speakTitle);
-
-const speakSub = document.createElement('p');
-speakSub.classList = 'speaker-subt';
-speakSub.innerHTML = speaker4[2].badge1;
-speak.appendChild(speakSub);
-
-const speakerDescr = document.createElement('p');
-speakerDescr.classList = 'speaker-descript';
-speakerDescr.innerHTML = speaker4[3].badge2;
-speak.appendChild(speakerDescr);
-
-speaker5.classList = 'speaker 5';
-cards.appendChild(speaker5);
-
-square.classList = 'square';
-cards.appendChild(square);
-
-const img = document.createElement('img')
-img.classList = 'dot';
-square.src = (speaker5[4].image);
-square.appendChild(img);
-
-const imgSP5 = document.createElement('img');
-imgSP5.classList = 'speakerimg';
-imgSP5.src = (speaker5[1].image);
-cards.appendChild(imgSP5);
-
-const speak = document.createElement('div');
-speak.classList = 'speak';
-speaker1.appendChild(speak);
-
-const speakTitle = document.createElement('h5');
-speakTitle.classList = 'speaker-title';
-speakTitle.innerText = speaker5[0].heading
-speak.appendChild(speakTitle);
-
-const speakSub = document.createElement('p');
-speakSub.classList = 'speaker-subt';
-speakSub.innerHTML = speaker5[2].badge1;
-speak.appendChild(speakSub);
-
-const speakerDescr = document.createElement('p');
-speakerDescr.classList = 'speaker-descript';
-speakerDescr.innerHTML = speaker5[3].badge2;
-speak.appendChild(speakerDescr);
-
-document.body.appendChild(main); 
+${objects[i].property}
+  */}
